f344c6cbfd11a0482b9be90f53cde85fa5c6645d
Mike Argento <mikeargento@gmail.com>
2026-03-14 11:23:25 -0400

    Remove serverExternalPackages to fix Vercel build

--- website/next.config.ts
+++ website/next.config.ts
@@ -1,7 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  serverExternalPackages: ["occproof", "@noble/hashes"],
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
